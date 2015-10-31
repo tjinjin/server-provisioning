19e3ffcedccf39fc5f522f6c702dc98fa99a475f
tjinjin <tjinjinprogram@gmail.com>
2015-10-31 17:17:42 +0900

    Fix

--- script/decrypt.sh
+++ script/decrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-. ~/server-provisioning/script/env.sh
+PUBLIC_KEY="pub.pem"
+SECRET_KEY=~/.ssh/id_gcp-circleci
+COMMON_KEY=".secret"
+FILE="gcp_key.json"
 
-echo $SECRET_KEY
-echo $COMMON_KEY
-echo $FILE
 openssl rsautl -decrypt -inkey $SECRET_KEY -in $COMMON_KEY.encrypt -out $COMMON_KEY
 
 openssl aes-256-cbc -d -in $FILE.encrypt -out $FILE -pass file:$COMMON_KEY
--- script/env.sh
+++
@@ -1,5 +0,0 @@
-HOME="~/server-provisioning"
-PUBLIC_KEY="$HOME/pub.pem"
-SECRET_KEY=~/.ssh/id_gcp-circleci
-COMMON_KEY="$HOME/.secret"
-FILE="$HOME/gcp_key.json"
