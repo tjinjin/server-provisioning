71b54e354a5a7b1824e7b5b7c2da7cb2daca7040
tjinjin <tjinjinprogram@gmail.com>
2015-10-31 17:13:24 +0900

    debug

--- script/decrypt.sh
+++ script/decrypt.sh
@@ -2,6 +2,9 @@
 
 . ~/server-provisioning/script/env.sh
 
+echo $SECRET_KEY
+echo $COMMON_KEY
+echo $FILE
 openssl rsautl -decrypt -inkey $SECRET_KEY -in $COMMON_KEY.encrypt -out $COMMON_KEY
 
 openssl aes-256-cbc -d -in $FILE.encrypt -out $FILE -pass file:$COMMON_KEY
